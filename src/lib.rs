c4cca9572728bda1385a10b69e513554edafba51
Martin Zeller <mz.bremerhaven@gmail.com>
2024-03-01 00:27:41 +0100

    Basic ControlCommand functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,65 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use bytes::Bytes;
+use ndn_protocol::{GenericNameComponent, Interest, Name};
+use ndn_tlv::{NonNegativeInteger, Tlv, TlvEncode};
+
+#[derive(Default, Debug, Tlv, Clone)]
+#[tlv(104)]
+pub struct ControlParameters {
+    name: Option<Name>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug, Tlv, Clone, Copy)]
+#[tlv(102)]
+pub struct StatusCode {
+    code: NonNegativeInteger,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Debug, Tlv, Clone)]
+#[tlv(103)]
+pub struct StatusText {
+    text: Bytes,
+}
+
+#[derive(Debug, Tlv, Clone)]
+#[tlv(101)]
+pub struct ControlResponse<T> {
+    status_code: StatusCode,
+    status_text: StatusText,
+    body: T,
+}
+
+pub fn make_command(module: &str, method: &str, params: ControlParameters) -> Option<Interest<()>> {
+    let mut name = Name::from_str(&format!("ndn:/localhost/nfd/{module}/{method}")).ok()?;
+    name.components
+        .push(GenericNameComponent::new(params.encode()).into());
+
+    let mut ret = Interest::<()>::new(name);
+    ret.set_must_be_fresh(true);
+
+    Some(ret)
+}
+
+impl ControlParameters {
+    pub fn new() -> Self {
+        ControlParameters { name: None }
+    }
+
+    pub fn set_name(mut self, name: Name) -> Self {
+        self.name = Some(name);
+        self
+    }
+}
+
+impl<T> ControlResponse<T> {
+    pub fn status_code(&self) -> NonNegativeInteger {
+        self.status_code.code
+    }
+
+    pub fn status_text(&self) -> &Bytes {
+        &self.status_text.text
+    }
+
+    pub fn body(&self) -> &T {
+        &self.body
     }
 }
